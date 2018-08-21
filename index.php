04fffb763020bde96816f25b06f7d4a1feba0700
unknown <nine4567@gmail.com>
2018-08-21 16:59:40 +0700

    make it better

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>My Portfolio</title>
+	<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat">
+</head>
+<body>
+
+	<div class="header-container">
+		<header>My Name is Parinyakiat Sukprasert</header>
+
+		<nav>
+			<ul>
+				<li class="border"><a href="index.html">About</a></li>
+				<li class="border"><a href="portfolio.html">Portfolio</a></li>
+			</ul>
+		</nav>
+	</div>
+	
+
+	<div class="main-container fadeDox">
+		<div class="row">
+			<div class="col-lg-7">
+					<section id="main-bio">
+							<h2>About Me</h2>
+							<div class="row">
+								<div class="col-md-3"><div class="img-profile"></div></div>
+								<div class="col-md-9"><p>
+									I'm Student at faculty of informatics
+									<p style="background-color:#4aaaa5;color:#fff;padding-left: 10px;">Skill</p>
+									<div>
+										<div class="row">
+											<div class="col-md-3" style="padding-left: 25px;">HTML</div>
+											<div class="col-md-9">
+												<div class="progress">
+													<div class="progress-bar" role="progressbar" style="width: 85%;background-color: #ff9900;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">85%</div>
+												  </div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-3" style="padding-left: 25px;">CSS</div>
+											<div class="col-md-9">
+												<div class="progress">
+													<div class="progress-bar" role="progressbar" style="width: 78%;background-color:#ff6a00;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">78%</div>
+												  </div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-3" style="padding-left: 25px;">Javascript</div>
+											<div class="col-md-9">
+												<div class="progress">
+													<div class="progress-bar" role="progressbar" style="width: 69%;background-color:#ff5200;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">69%</div>
+												  </div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-3" style="padding-left: 25px;">PHP</div>
+											<div class="col-md-9">
+												<div class="progress">
+													<div class="progress-bar" role="progressbar" style="width: 64%;background-color:#ff5200;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">64%</div>
+												  </div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-3" style="padding-left: 25px;">Java</div>
+											<div class="col-md-9">
+												<div class="progress">
+													<div class="progress-bar" role="progressbar" style="width: 78%;background-color:#ff6a00;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">78%</div>
+												  </div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-3" style="padding-left: 25px;">C++</div>
+											<div class="col-md-9">
+												<div class="progress">
+													<div class="progress-bar" role="progressbar" style="width: 55%;background-color:#ff2f00;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">55%</div>
+												  </div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-3" style="padding-left: 25px;">C#</div>
+											<div class="col-md-9">
+												<div class="progress">
+													<div class="progress-bar" role="progressbar" style="width: 63%;background-color:#ff5200;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">63%</div>
+												  </div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-3" style="padding-left: 25px;">Python</div>
+											<div class="col-md-9">
+												<div class="progress">
+													<div class="progress-bar" role="progressbar" style="width: 58%;background-color:#ff2f00;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">58%</div>
+												  </div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-3" style="padding-left: 25px;">Swift</div>
+											<div class="col-md-9">
+												<div class="progress">
+													<div class="progress-bar" role="progressbar" style="width: 70%;background-color:#ff6a00;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">70%</div>
+												  </div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-3" style="padding-left: 25px;">TypeScript</div>
+											<div class="col-md-9">
+												<div class="progress">
+													<div class="progress-bar" role="progressbar" style="width: 66%;background-color:#ff5200;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">66%</div>
+												  </div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-3" style="padding-left: 25px;">Kotlin</div>
+											<div class="col-md-9">
+												<div class="progress">
+													<div class="progress-bar" role="progressbar" style="width: 73%;background-color:#ff6a00;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">73%</div>
+												  </div>
+											</div>
+										</div>
+									</div>
+								</p></div>
+							</div>
+						</section>
+			</div>
+			<div class="col-lg-5">
+					<section id="social-links">
+							<h3>Connect with Me</h3>
+							<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D1057997304249848%26set%3Da.124477434268511%26type%3D3%26theater&width=400&show_text=true&height=569&appId" width="400" height="569" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+					  </section>
+			</div>
+		</div>
+		
+
+    	
+	</div>
+	<!-- Don't remove-->
+	<footer>&#64; Copyright 2017 Designed by Parinyakiat Sukprasert</footer>
+	<script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
